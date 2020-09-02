7476acdf06291fd95ea861aba7fcfde8115b8c79
felipevianna <felipevianna@unirn.edu.br>
2020-09-01 21:10:32 -0300

    Criação do componente Conversor.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "bootstrap-css-only";
 import "./App.css";
 import Header from "./components/Header";
+import Conversor from "./components/Conversor";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Conversor />
     </div>
   );
 }
---
+++ src/components/Conversor.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Conversor = (props) => {
+  return (
+    <div className="content">
+      <div className="row justify-content-md-center">
+        <div className="card text-center w-50">
+          <div className="card-header bg-info">Câmbio Natal</div>
+          <div className="card-body">Formulário</div>
+          <div className="card-footer text-light bg-dark">R$ 0.00</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Conversor;
